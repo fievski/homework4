9bb22edb4ad50e6ab6e548af1f22bc113fd6519c
fievski <fievski@yandex.ru>
2024-06-06 13:20:47 +0600

    Add files via upload

---
+++ homework4.py
@@ -0,0 +1,24 @@
+my_string = input("Введите Ваше имя: ")  # Организация программ и методы строк.
+print(len(my_string))
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace( ' ', ''))
+print(my_string[0:1])
+print(my_string[-1:])
+
+
+
+
+
+
+
+
+
+
+# name = input ("Введите Ваше имя: ") # Организация программ и методы строк.
+# current_year = 2024
+# date_of_birth = int(input("В какой году Вы родились?"))
+# age = current_year - date_of_birth
+# print("Здравствуйте,", name)
+# print("В этом году Вам", age, "лет")
+#print('Привет, я строка в нижнем регистре'.)
